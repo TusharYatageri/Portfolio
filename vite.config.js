a45febbcd19ad21b2d19ba2385625dbbd1a38665
TusharYatageri <yatageritushar02@gmail.com>
2025-07-30 02:15:49 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MyPortfolio/', // 👈 must match your GitHub repo name exactly
-  plugins: [react()]
+  base: '/Portfolio/',
+  plugins: [react()],
 })
