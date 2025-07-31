26df725564e9d4ca66103e095c3064bf8ab12f40
TusharYatageri <yatageritushar02@gmail.com>
2025-07-31 16:29:27 +0530

    Added contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import SkillSection from './components/SkillSection';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <AboutSection/>
       <SkillSection/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../cssComponents/Contact.css'; 
+
+function Contact() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5e7e6ec8-9c53-4427-93b0-35fa789cc7fb");
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert("✅ Message sent successfully!");
+      event.target.reset();
+    } else {
+      alert("❌ Failed to send message.");
+    }
+  };
+
+  return (
+    <section className="contact-section" id="contact">
+      <h2>Contact Me</h2>
+      <form onSubmit={onSubmit} className="contact-form">
+        <input type="text" name="name" placeholder="Your Name" required />
+        <input type="email" name="email" placeholder="Your Email" required />
+        <input type="text" name="phone" id="phone" placeholder="Your Contact Number"/>
+        <input type="text" name="name"  placeholder="Your subject" required />
+        <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
